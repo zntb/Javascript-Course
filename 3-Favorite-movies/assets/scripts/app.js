d61edb8b4abbbe3fed2f0d3fe9b4ff686df66ae4
zntb <zenetibi@yahoo.com>
2023-11-08 17:48:44 +0200

    Add control for deleting contents

--- 3-Favorite-movies/assets/scripts/app.js
+++ 3-Favorite-movies/assets/scripts/app.js
@@ -16,7 +16,21 @@ const updateUI = () => {
   }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1);
+  const listRoot = document.getElementById('movie-list');
+  listRoot.children[movieIndex].remove();
+  //  listRoot.removeChild(listRoot.children[movieIndex]); - Alternative to remove
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement('li');
   newMovieElement.className = 'movie-element';
   newMovieElement.innerHTML = `
@@ -28,6 +42,7 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
         <p>${rating}/5 stars</p>
     </div>       
   `;
+  newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
   const listRoot = document.getElementById('movie-list');
   listRoot.append(newMovieElement);
 };
@@ -69,6 +84,7 @@ const addMovieHandler = () => {
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -78,7 +94,12 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleMovieModal();
   clearMovieInput();
-  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
   updateUI();
 };
 
