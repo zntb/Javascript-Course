3c425824d6458e9c3971664824ab9ba6d27bf27a
zntb <zenetibi@yahoo.com>
2023-11-10 12:59:55 +0200

    Add filter functionality

--- 4-Movies-project-2/assets/scripts/objects.js
+++ 4-Movies-project-2/assets/scripts/objects.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
 
   if (movies.length === 0) {
@@ -14,9 +14,13 @@ const renderMovies = () => {
   }
   movieList.innerHTML = '';
 
-  movies.forEach((movie) => {
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    let text = movie.info.title + '-';
+    let text = movie.info.title + ' - ';
     for (const key in movie.info) {
       if (key !== 'title') {
         text += `${key}: ${movie.info[key]}`;
@@ -52,4 +56,10 @@ const addMovieHandler = () => {
   renderMovies();
 };
 
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
