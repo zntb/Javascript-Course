f8d9bc43e03958169ce59701ac8a06ba982567ee
zntb <zenetibi@yahoo.com>
2023-11-11 14:56:36 +0200

    Add getters and setters and update total cart price

--- 5-Dummy-Shop-Project-OOP/assets/scripts/app.js
+++ 5-Dummy-Shop-Project-OOP/assets/scripts/app.js
@@ -15,9 +15,25 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
   addProduct(product) {
-    this.items.push(product);
-    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
   }
 
   render() {
